29296ec7072c0ae628b2ee06e6849db50743203a
DSK2007 <202112045@office.induk.ac.kr>
2023-04-05 14:40:32 +0900

    Initial commit

---
+++ src/main/java/idusw/springboot/boardthymeleaf/repository/MemoRepository.java
@@ -0,0 +1,7 @@
+package idusw.springboot.boardthymeleaf.repository;
+
+import idusw.springboot.boardthymeleaf.entity.Memo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+}
